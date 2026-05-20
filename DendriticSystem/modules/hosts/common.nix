de2ebd82edf8a90846e3ae541488aa7c9c73cc87
scott <scott.ledyard1@gmail.com>
2026-05-19 17:32:33 -0700

    updates

--- DendriticSystem/modules/hosts/common.nix
+++ DendriticSystem/modules/hosts/common.nix
@@ -14,6 +14,10 @@
   flake.modules.nixos.hardware-common-bootloader = {
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.systemd-boot.extraEntries."windows.conf" = ''
+      title Windows
+      efi /EFI/Microsoft/Boot/bootmgfw.efi
+    '';
   };
 
   flake.modules.nixos.hardware-common-connectivity = {
--- DendriticSystem/modules/hosts/pharloom.nix
+++ DendriticSystem/modules/hosts/pharloom.nix
@@ -13,24 +13,33 @@
     flake.modules.nixos.hardware-pharloom-disks = {
         fileSystems."/" =
             { 
-                device = "/dev/disk/by-uuid/058da0c9-9e7d-4c79-8f3a-8652bbb5609b";
+                device = "/dev/disk/by-uuid/afc0760b-10de-431b-9941-187723287181";
                 fsType = "ext4";
             };
 
         fileSystems."/boot" =
             { 
-                device = "/dev/disk/by-uuid/F2B8-D2B1";
+                device = "/dev/disk/by-uuid/C1B2-0527";
                 fsType = "vfat";
-                options = [ "fmask=0022" "dmask=0022" ];
+                options = [ "fmask=0077" "dmask=0077" ];
             };
 
-        swapDevices = [ ];
+        swapDevices = 
+          [ { device = "/dev/disk/by-uuid/0d1b5e4e-9c0a-4162-a420-e2086fc05524"; }
+          ];
     };
 
-    flake.modules.nixos.hardware-pharloom-hardware = { lib, ... }: {
-        imports = [ ];
+    flake.modules.nixos.hardware-pharloom-hardware = { config, lib, modulesPath, ... }: {
+        imports = 
+          [ (modulesPath + "/installer/scan/not-detected.nix") 
+          ];
         nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-        virtualisation.virtualbox.guest.enable = true;
+        hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+        hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+        hardware.nvidia.open = true;
+        services.xserver.videoDrivers = [ "nvidia" ];
+	hardware.nvidia.modesetting.enable = true;
+        hardware.graphics.enable = true;
     };
 
     flake.modules.nixos.hardware-pharloom-connectivity = {
@@ -38,7 +47,7 @@
     };
 
     flake.modules.nixos.hardware-pharloom-kernel = {
-        boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
+        boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" ];
         boot.initrd.kernelModules = [ ];
         boot.kernelModules = [ "kvm-amd" ];
         boot.extraModulePackages = [ ];        
